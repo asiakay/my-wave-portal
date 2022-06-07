431b1075ef6e9f48bbd9f9460c8f5caf5f88243e
Asia K <asialakaygrady@gmail.com>
2022-06-07 16:48:43 -0400

    initial commit

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.7.3;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    constructor(){
-        console.log("Greetings, I am a smart little contract.. ;)");
+    uint256 totalWaves;
+
+
+    constructor() {
+        console.log("We are tomorrow. We are the builders of our dreams.");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,34 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // compiles the contract and generates files under the artifacts directory 
-    const waveContract = await waveContractFactory.deploy(); // getting hardhat to create a local Ethereum network for this contract that will self destruct after the script completes 
-    await waveContract.deployed(); // waiting until the contrct is deployed to the local blockchain
-    console.log("Contract deployed to:", waveContract.address); // once deployed, "waveContract.address" will give the address of the deployed contract
-};
-
-const runMain = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+  
+    console.log('Contract deployed to:', waveContract.address);
+    console.log('Contract deployed by:', owner.address);
+  
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+  
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+  
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+  };
+  
+  const runMain = async () => {
     try {
-        await main();
-        process.exit(0); // exit Node process without error
-    } catch (error){
-        console.log(error);
-        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
     }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-};
-
-runMain();
+  };
+  
+  runMain();
